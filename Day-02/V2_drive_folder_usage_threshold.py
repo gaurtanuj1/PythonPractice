12293844d7ef3740bc38b76394f5c0bcede4a2e6
gaurtanuj1 <gaurtanuj1@gmail.com>
2026-09-20 06:16:27 +0530

    added V2 drive folder usage threshold

---
+++ Day-02/V2_drive_folder_usage_threshold.py
@@ -0,0 +1,42 @@
+##Import OS module ---------------------------------------------------------------------------------
+
+import os
+
+folders_to_monitor = [
+    r"D:\Oracle",
+    r"D:\Project",
+    r"D:\Study Pack",
+    r"D:\TOOLS-SOFTWARES"   
+]
+
+threshold_gb = 0.5
+
+##Define a function to get the size of a folder in bytes -------------------------------------------
+
+def get_folder_size(folder_path):
+    total_size = 0
+
+    for root, folders, files in os.walk(folder_path):
+
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_size = os.path.getsize(file_path)
+            total_size += file_size
+    
+    return total_size
+
+##Define a function to convert size(bytes) in human readable format = GB ----------------------------
+
+def bytes_to_gb(size):
+    return size/(1024**3)
+
+for folder in folders_to_monitor:
+    size_bytes = get_folder_size(folder)
+    size_gb = bytes_to_gb(size_bytes)
+
+    print(f"{folder} : {size_gb: .2f} GB")
+
+    if size_gb > threshold_gb:
+        print(f"{folder} has exceeded the threshold of {threshold_gb} GB. Current size: {size_gb: .2f} GB")
+    else:
+        print(f"{folder} is within limit.")
