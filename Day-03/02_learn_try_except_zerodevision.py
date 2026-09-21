dafba44cb84dfb4ede6c36473d2fc8ba69688930
gaurtanuj1 <gaurtanuj1@gmail.com>
2026-09-21 21:50:27 +0530

    Try Except Zerodevision handling

--- Day-03/02_learn_try_except_zerodevision.py
+++ Day-03/02_learn_try_except_zerodevision.py
@@ -1,3 +1,6 @@
+'''
+# this program will throw an error if the user enters 0 as input.
+
 number = int(input("Enter a number: "))
 
 result = 100/number
@@ -5,3 +8,15 @@ result = 100/number
 print(f"Result is: {result}")
 
 print("Program Complete.")
+
+'''
+
+try:
+    number = int(input("Enter a number: "))
+    result = 100/number
+    print(f" Answer is: {result}")
+
+except ZeroDivisionError:
+    print("You cannot divide by zero.")
+
+
