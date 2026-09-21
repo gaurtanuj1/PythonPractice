ac0704d94064a8a89010f4fa8a53d6319ab3e705
gaurtanuj1 <gaurtanuj1@gmail.com>
2026-09-21 21:25:43 +0530

    Learn try except handling in python

---
+++ Day-03/01_learn_try_except_basic.py
@@ -0,0 +1,8 @@
+try:
+    number = int(input("Enter a number: "))
+    print(f"you've entered: {number}")
+
+except:
+    print("Something went wrong.")
+
+print("Program Complete.")
---
+++ Day-03/02_learn_try_except_zerodevision.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+
+result = 100/number
+
+print(f"Result is: {result}")
+
+print("Program Complete.")
